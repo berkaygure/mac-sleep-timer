76869e3cf213af48be05f25e2448bd20b3f768c5
Berkay GÜRE <bgure@hotmail.com.tr>
2023-08-01 16:16:07 +0300

    Initial Commit

---
+++ SleepTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SleepTimer
+//
+//  Created by Berkay Güre on 1.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SleepTimer/SleepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimerApp.swift
+//  SleepTimer
+//
+//  Created by Berkay Güre on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
